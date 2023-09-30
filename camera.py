1342809f6203125a0e24ad3414d59e9e266e9c50
Neiv Malaviya <neiv.malaviya@gmail.com>
2023-09-30 19:03:21 +0530

    Red Rectangle | Inverted Camera

--- camera.py
+++ camera.py
@@ -1,50 +1,40 @@
 import cv2
 
-# Load the deep learning model
-modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
-configFile = "deploy.prototxt"
-net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
+# Load the Haar cascade file
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Open the webcam
-cap = cv2.VideoCapture(0)
-
-# Create window for display
-cv2.namedWindow('Webcam', cv2.WINDOW_NORMAL)
+cap0 = cv2.VideoCapture(1)
 
 while True:
-    # Read frame from the webcam
-    ret, frame = cap.read()
+    # Read frames from the webcam
+    ret0, frame0 = cap0.read()
 
-    # Check if the frame was read correctly
-    if not ret:
+    # Check if the frames were read correctly
+    if not ret0:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    # Prepare the frame for the model
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    # Flip the frame horizontally
+    frame0 = cv2.flip(frame0, 1)
+
+    # Convert the frames to grayscale
+    gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
 
     # Perform face detection
-    net.setInput(blob)
-    faces = net.forward()
+    faces0 = face_cascade.detectMultiScale(gray0, 1.1, 4)
 
     # Draw rectangle around the faces
-    for i in range(faces.shape[2]):
-        confidence = faces[0, 0, i, 2]
-        if confidence > 0.5:
-            box = faces[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
-            (x, y, w, h) = box.astype("int")
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for (x, y, w, h) in faces0:
+        cv2.rectangle(frame0, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-    # Resize the window
-    cv2.resizeWindow('Webcam', 480, 360)
-
-    # Display the resulting frame
-    cv2.imshow('Webcam', frame)
+    # Display the resulting frames
+    cv2.imshow('Webcam', frame0)
 
     # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the webcam and destroy all windows
-cap.release()
+cap0.release()
 cv2.destroyAllWindows()
